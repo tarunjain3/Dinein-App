9773093ac402f37cf246b5f553545319c0082025
Tarun jain <tarun.jain@viithiisys.com>
2021-12-09 12:43:55 +0530

    code-clean

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import '../widgets/meal_item.dart';
@@ -22,7 +21,6 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
 
   @override
   void initState() {
-    // ...
     super.initState();
   }
 
@@ -69,46 +67,5 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
     );
   }
 }
-// import 'package:flutter/material.dart';
 
-// import '../widgets/meal_item.dart';
-// import '../dummy_data.dart';
-
-// class CategoryMealsScreen extends StatelessWidget {
-//   static const routeName = '/category-meals';
-
-//   const CategoryMealsScreen({Key? key}) : super(key: key);
-//   // final String categoryId;
-//   // final String categoryTitle;
-
-//   // CategoryMealsScreen(this.categoryId, this.categoryTitle);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final routeArgs =
-//         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
-//     final categoryTitle = routeArgs['title'];
-//     final categoryId = routeArgs['id'];
-//     final categoryMeals = DUMMY_MEALS.where((meal) {
-//       return meal.categories.contains(categoryId);
-//     }).toList();
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(categoryTitle.toString()),
-//       ),
-//       body: ListView.builder(
-//         itemBuilder: (ctx, index) {
-//           return MealItem(
-//             title: categoryMeals[index].title,
-//             imageUrl: categoryMeals[index].imageUrl,
-//             duration: categoryMeals[index].duration,
-//             affordability: categoryMeals[index].affordability,
-//             complexity: categoryMeals[index].complexity,
-//           );
-//         },
-//         itemCount: categoryMeals.length,
-//       ),
-//     );
-//   }
-// }
 
